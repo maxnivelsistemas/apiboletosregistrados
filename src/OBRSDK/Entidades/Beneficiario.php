22b38e02035c5c1f23043a6609470e6cc5cc71d4
Antônio Junior <antoniojr75@outlook.com>
2017-07-20 10:08:03 -0300

    Iniciando projeto

---
+++ src/OBRSDK/Entidades/Beneficiario.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace OBRSDK\Entidades;
+
+/**
+ * Description of Beneficiario
+ *
+ * @author Antonio
+ */
+class Beneficiario extends Abstratos\AEntidadePropriedades {
+
+    ///
+    /// ATRIBUTOS OBRIGATORIOS
+    ///
+    public $agencia;
+    public $agencia_digito;
+    public $conta;
+    public $conta_digito;
+    public $nome_beneficiario;
+    public $documento_beneficiario;
+    public $endereco_beneficiario;
+    public $endereco_numero_beneficiario;
+    public $cep_beneficiario;
+    public $cidade_beneficiario;
+    public $uf_beneficiario;
+
+}
---
+++ src/OBRSDK/HttpClient/OAuth2Cliente.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace OBRSDK\HttpClient;
+
+/**
+ * Description of OAuth2Cliente
+ *
+ * @author Antonio
+ */
+class OAuth2Cliente extends Nucleo\ApiCliente {
+
+    /**
+     * Faz autenticacao na API com as suas credenciais de 'appId' e 'appSecret'
+     * Neste caso você consegue acessar seus dados
+     * 
+     * @throws \OBRSDK\Exceptions\RespostaException
+     * @throws \OBRSDK\Exceptions\AutenticacaoException
+     */
+    public function autenticarComCredenciais() {
+        $this->oauth([
+            'grant_type' => 'client_credentials',
+            'scope' => '*'
+        ]);
+    }
+
+    /**
+     * Faz autenticacao na API em nome de outro usuário utilizando um codigo de 
+     * autorização que esse usuário permite em ser utilizado por você
+     * 
+     * @param string $code
+     * @return mixed
+     * @throws OBRSDK\Exceptions\AutorizationException
+     * @throws \OBRSDK\Exceptions\RespostaException
+     */
+    public function autenticarComAutorizacao($code = null) {
+        $error = filter_input(INPUT_GET, 'error');
+        if ($error != null && $error != '') {
+            throw new \OBRSDK\Exceptions\AutorizationException($error, filter_input(INPUT_GET, 'error_descript'));
+        }
+
+        $authorizationCode = $code == null ? filter_input(INPUT_GET, 'code') : $code;
+        $this->oauth([
+            'grant_type' => 'authorization_code',
+            'code' => $authorizationCode
+        ]);
+
+        return filter_input(INPUT_GET, 'state');
+    }
+
+    /**
+     * Se um access_token com permissoes de um usuário foi expirado e você tiver 
+     * um reflesh token do mesmo, você pode resolicitar uma nova autenticacao
+     * utilizando o reflesh token
+     * 
+     * @param string $refreshToken
+     * @throws \OBRSDK\Exceptions\RespostaException
+     * @throws \OBRSDK\Exceptions\AutenticacaoException
+     * @return \OBRSDK\ObjetoBoletosRegistrados
+     */
+    public function autenticarComRefreshToken($refreshToken) {
+        $this->oauth([
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $refreshToken
+        ]);
+    }
+
+    /**
+     * 
+     * @param type $config
+     * @return \OBRSDK\ObjetoBoletosRegistrados
+     * @throws \OBRSDK\Exceptions\AutenticacaoException
+     * @throws \OBRSDK\Exceptions\RespostaException
+     */
+    private function oauth($config) {
+        try {
+            $param = array_merge(
+                    $config, [
+                'client_id' => $this->getInstancia()->getAppId(),
+                'client_secret' => $this->getInstancia()->getAppSecret()
+                    ]
+            );
+
+            $response = $this->postParam('auth/token', $param)->getResposta(true);
+
+            if (!isset($response['refresh_token'])) {
+                $response['refresh_token'] = null;
+            }
+            $response['data_token'] = time();
+
+            $entidadeAccessToken = new \OBRSDK\Entidades\AccessToken();
+            $entidadeAccessToken->setAtributos($response);
+
+            $this->getInstancia()->setObjAccessToken($entidadeAccessToken);
+        } catch (\OBRSDK\Exceptions\RespostaException $ex) {
+            if (isset($ex->getError()->OAuth2->erro_description)) {
+                throw new \OBRSDK\Exceptions\AutenticacaoException($ex);
+            } else {
+                throw $ex;
+            }
+        }
+    }
+
+}
